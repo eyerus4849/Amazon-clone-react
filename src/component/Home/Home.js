4c52c5ecec4286217c19cf722b0519053fe28c1f
eyerus4948 <eyerus.girma.54@gmail.com>
2021-08-04 23:19:32 -0400

    Carousel added in home component

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,63 +1,101 @@
 import React,{useState} from 'react'
 import './home.css'
 import Product from './Product'
-import NavigateNextIcon from '@material-ui/icons/NavigateNext';
-import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
+// import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+// import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Carousel} from 'react-bootstrap'
 function Home() {
-  let images=["https://images-na.ssl-images-amazon.com/images/G/01/xcm/brand_truths/4_29_Update/C_TBDB_SMB_GW_hero_desktop_3000x1200_2x._CB670680788_.jpg","https://images-na.ssl-images-amazon.com/images/G/01/digital/video/merch/2021/Other/RB-3968_SVOD_MultiTitle_GW_Test/AMZN_RB-3968_SVOD_MultiTitle_GW_Control_EN_GWBleedingHero_V2_3000x1200._CB668890782_.jpg","https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2021/img/Events/BTS/Homepage/2021_BTS_DesktopHero_3000x1200_Phase1_2x._CB669825959_.jpg","https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/Nzg3NzIxZDct/Nzg3NzIxZDct-OWNiZTMxYTAt-w3000._CB664037480_.jpg","https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/ZWUxZjhhODUt/ZWUxZjhhODUt-MGZhOGZmZjQt-w3000._CB663484184_.jpg"]
+  // let images=["https://images-na.ssl-images-amazon.com/images/G/01/xcm/brand_truths/4_29_Update/C_TBDB_SMB_GW_hero_desktop_3000x1200_2x._CB670680788_.jpg","https://images-na.ssl-images-amazon.com/images/G/01/digital/video/merch/2021/Other/RB-3968_SVOD_MultiTitle_GW_Test/AMZN_RB-3968_SVOD_MultiTitle_GW_Control_EN_GWBleedingHero_V2_3000x1200._CB668890782_.jpg","https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2021/img/Events/BTS/Homepage/2021_BTS_DesktopHero_3000x1200_Phase1_2x._CB669825959_.jpg","https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/Nzg3NzIxZDct/Nzg3NzIxZDct-OWNiZTMxYTAt-w3000._CB664037480_.jpg","https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/ZWUxZjhhODUt/ZWUxZjhhODUt-MGZhOGZmZjQt-w3000._CB663484184_.jpg"]
   
-  const[count,Setcount]=useState(1)
-  const[image,Setimage]=useState(images[0])
-  const change_imge_next=()=>{
-    Setcount(count=>count+1)
-    if(count==5){
-      Setcount(1)
-      Setimage(images[0])
-    }
-   else if(count<5){
-      Setimage(images[count])
-    } 
-}
-  const change_imge_back=()=>{
-    Setcount((count)=>count-1)
-    if(count==1){
-      Setcount(4)
-      Setimage(images[4])
-    }
-    if(count>1){
-      Setimage(images[count-2])
-    }
-  }
+//   const[count,Setcount]=useState(1)
+//   const[image,Setimage]=useState(images[0])
+//   const change_imge_next=()=>{
+//     Setcount(count=>count+1)
+//     if(count==5){
+//       Setcount(1)
+//       Setimage(images[0])
+//     }
+//    else if(count<5){
+//       Setimage(images[count])
+//     } 
+// }
+//   const change_imge_back=()=>{
+//     Setcount((count)=>count-1)
+//     if(count==1){
+//       Setcount(4)
+//       Setimage(images[4])
+//     }
+//     if(count>1){
+//       Setimage(images[count-2])
+//     }
+//   }
   
     return (
         <div className="home">
            
             <div className="home__container">
-    <div className="home__image" style={{
+    {/* <div className="home__image" style={{
         backgroundSize: "cover",
         backgroundImage: `url(${image})`,
         height:"400px",
         backgroundRepeat:"no-repeat"
       }}>
-         {/* <img
-          className="home__image"
-          src={image}
-          alt=""
-        /> */}
+        
         <div className="navigate">
         <div className="img_bttn_back" onClick={change_imge_back}>< NavigateBeforeIcon style={{ fontSize: 60 }} /></div>
         <div className="img_bttn" onClick={change_imge_next}>< NavigateNextIcon style={{ fontSize: 60 }} /></div>
         </div>
-        </div>
+        </div> */}
+        <Carousel fade={true} pause={false} indicators={false} className="home__image">
+      <Carousel.Item interval={4000}>
+        <img
+          className="d-block w-100 "
+          src="https://images-na.ssl-images-amazon.com/images/G/01/xcm/brand_truths/4_29_Update/C_TBDB_SMB_GW_hero_desktop_3000x1200_2x._CB670680788_.jpg"
+          alt="First slide"
+        />
+       
+      </Carousel.Item>
+      <Carousel.Item interval={4000}>
+        <img
+          className="d-block w-100 "
+          src="https://images-na.ssl-images-amazon.com/images/G/01/digital/video/merch/2021/Other/RB-3968_SVOD_MultiTitle_GW_Test/AMZN_RB-3968_SVOD_MultiTitle_GW_Control_EN_GWBleedingHero_V2_3000x1200._CB668890782_.jpg"
+          alt="2 slide"
+        />
+        
+      </Carousel.Item>
+      <Carousel.Item interval={4000}>
+        <img
+          className="d-block w-100"
+         src="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/Y2UwYWM0MDQt/Y2UwYWM0MDQt-OTZkN2NjYTIt-w1500._CB668789270_.jpg"
+          alt="3 slide"
+        />
+       
+      </Carousel.Item>
+      <Carousel.Item interval={4000}>
+        <img
+          className="d-block w-100"
+         src="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/ZWUxZjhhODUt/ZWUxZjhhODUt-MGZhOGZmZjQt-w3000._CB663484184_.jpg"
+          alt="3 slide"
+        />
+       
+      </Carousel.Item>
+      <Carousel.Item interval={4000}>
+        <img
+          className="d-block w-100"
+         src="https://images-na.ssl-images-amazon.com/images/G/01/digital/video/merch/Movie/CIND_2021_GWBleedingHero_1500x600_PRE_Final_en-US_PVD7333_Cinderella._CB664771797_.jpg"
+          alt="3 slide"
+        />
+       
+      </Carousel.Item>
+    </Carousel>
            <div className="home__row">
-        {/* <div className="img_bttn" onClick={change_imge_next}>< NavigateNextIcon style={{ fontSize: 60 }} /></div> */}
-   
        <Product
         id="49538094"
-        title="Acer Aspire 5 Slim Laptop, 15.6 inches Full HD IPS Display, AMD Ryzen 3 3200U, Vega 3 Graphics, 4GB DDR4, 128GB SSD, Backlit Keyboard, Windows 10 in S Mode, A515-43-R19L, Silver"
-        price={406.99}
-        rating={4}
-        image="https://m.media-amazon.com/images/I/41vMYgD92xL._AC_SY322_.jpg"
+        title="SUNBURY 5-Piece Outdoor Sectional Brown Wicker Sofa, PE Rattan Patio Furniture Set w Blue Cushion, Tempered Glass Table, Weatherproof Cover for Backyard"
+        price={749.80}
+        rating={5}
+        image="https://m.media-amazon.com/images/I/71hLEmVb8lL._AC_UL800_FMwebp_QL65_.jpg"
        />
        <Product
         id="12321341"
@@ -150,7 +188,22 @@ function Home() {
         image="https://m.media-amazon.com/images/I/71owWDr763L._AC_UL800_FMwebp_QL65_.jpg"
        />
         </div>
-        
+        <div className="home__row">
+       <Product
+        id="41785"
+        title="FEZIBO Dual Motor Height Adjustable Electric Standing Desk with Keyboard Tray, 55 x 24 Inch Sit Stand Table with Splice Board, Black Frame/Black and Rustic Brown Top"
+        price={339.99}
+        rating={4}
+        image="https://m.media-amazon.com/images/I/71b5IKBxZ9S._AC_SL1500_.jpg"
+       />
+       <Product
+        id="8547963"
+        title="Patio Wicker Furniture Set 6 Pieces Outdoor PE Rattan Conversation Couch Sectional Chair Sofa Set with Turquoise Cushion"
+        price={899.99}
+        rating={5}
+        image="https://m.media-amazon.com/images/I/81uDGDoj60S._AC_UL800_FMwebp_QL65_.jpg"
+       />
+        </div>
             </div>
             
         </div>
